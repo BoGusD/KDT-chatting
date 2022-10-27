aa24af04478bb7f8bc513580e845aedef3cacc9c
unknown <qazxwd44@hanmail.net>
2022-10-27 14:41:34 +0900

    채팅 구현

---
+++ chat/public/mongo.js
@@ -0,0 +1,14 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// 'mongodb+srv://tetz:qwer1234@cluster0.sdiakr0.mongodb.net/?retryWrites=true&w=majority';
+
+const uri =
+  'mongodb+srv://tetz:qwer1234@cluster0.sdiakr0.mongodb.net/?retryWrites=true&w=majority';
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+module.exports = client;
